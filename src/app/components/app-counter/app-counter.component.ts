2eb403105158b627f79a8fbb8605d625a91611a0
Francisco Vaz <ftomasvaz@gmail.com>
2020-11-19 17:52:38 +0000

    generate counter component

---
+++ src/app/components/app-counter/app-counter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  templateUrl: './app-counter.component.html',
+  styleUrls: ['./app-counter.component.css']
+})
+export class AppCounterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
