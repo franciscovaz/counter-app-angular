82880ae80fe25ab33f733aada0f7d6b26e7a2002
Francisco Vaz <ftomasvaz@gmail.com>
2020-11-19 18:15:47 +0000

    implementing increment and decrement functions. One way data binding

--- src/app/components/app-counter/app-counter.component.ts
+++ src/app/components/app-counter/app-counter.component.ts
@@ -3,13 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-counter',
   templateUrl: './app-counter.component.html',
-  styleUrls: ['./app-counter.component.css']
+  styleUrls: ['./app-counter.component.css'],
 })
 export class AppCounterComponent implements OnInit {
+  counter: number;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.counter = 0;
   }
 
+  increment(): void {
+    this.counter++;
+  }
+
+  decrement() {
+    this.counter--;
+  }
 }
