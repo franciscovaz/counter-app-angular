4dd82f09ae8249ffbad921b89e6828e32472bd63
Francisco Vaz <ftomasvaz@gmail.com>
2020-11-19 17:51:51 +0000

    generate footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppHeaderComponent } from './components/app-header/app-header.component';
+import { AppFooterComponent } from './components/app-footer/app-footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppHeaderComponent
+    AppHeaderComponent,
+    AppFooterComponent
   ],
   imports: [
     BrowserModule
